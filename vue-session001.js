c09dd3fcca6838831dfa8fb00d62690a58a705e8
Ranjan <mail2ranjan.kumar@gmail.com>
2025-05-05 14:01:00 +0530

    Create vue-session001.js

---
+++ vue-session001.js
@@ -0,0 +1,30 @@
+// Q1: Explain VueJS lifecycle hooks. Explain with code example.
+// You answer here:
+
+// Q2: What is computer properties? How is it different from methods? When we should use computer properties? Explain with example.
+
+// Q3: What is composition API? What problems it handles? How is it better then option-api?
+
+// Q4: Write an vue component that has two inputs and a button. On click of buttons it adds up the number given in inputs. Input should be text type and on click of button you shoudl validate for number.
+//     If sum exceed 100, show the result in red else in green
+
+// Q5: What vue js lifecycle is good for API call?
+
+// Q5: How to manage state in vue js single page applicaton? 
+
+// Q6: Write is pinia storage class example
+
+// Q7: What is lazy loading?
+
+// Q8: What is watcher? When to use them? Give an example in composition API.
+
+// Q9: What are main improvements in vue3 over vue 2?
+
+// Q10: If we want to perform a taks and stop the HTML rendering, what vue lifecyle hook should be used?
+
+// Q11: Wrtie is vue JS component that allow filtering array of string. The component should have a search text box and table to show the search result. Make any randon string array for this POC.
+
+// Q12: Differance between v-if and v-show? 
+
+// Q13: Write a vue js component, that do a rest call on page load and renders the content on that page. Hit this API `https://api.restful-api.dev/objects` and render it in individual div in flow layout.
+// On click of individual div, it should call `https://api.restful-api.dev/objects?id=<<id returned as response>>` and display the data in new page.
