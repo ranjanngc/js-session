efbe69095a9f8a2faa62d27e281d35ca68b7bf9e
Ranjan <mail2ranjan.kumar@gmail.com>
2025-05-05 13:00:23 +0530

    Create js-001.js

---
+++ js-001.js
@@ -0,0 +1,75 @@
+//Q1:  You have a input array arr=[1,2,3], write your JS logic to output [1x3, 1x3, 1x2]
+// You code should go here-
+
+
+
+// Q2: Is JavaScript multithreaded? How can you make multithread default?
+
+
+// Q3: What is defferance b/w let and const? Also explain with example.
+
+
+// Q4: What is REST? Write a simple rest api call using `fetch` API. Use these rest endpint https://restful-api.dev/ and write GET, POST, PUT and DELETE call.
+
+
+// Q5: What is cookies? Advantanges and disadvantages of cookies. Wrtite a simple example to read and write into cookies.
+
+
+// Q6: What is webstorage? Advantanges and disadvantages of webstorage. Wrtite a simple example to read and write into webstorage each localStorage & sessionStorage
+
+// @7: What are different DataTypes available in JS. Exaplin them with an example
+
+// Q8: What is Array.map()? Explain with a working example
+
+// Q9: What is Array.filter()? Exaplin with working example. What is differance with map() and filter() function.
+
+// Q10: List 5 Array methods and explain with example (exclude filter and map)
+
+// Q11: Merge these arrays - 
+let a = [1, 4,10,12];
+let b = [2,3,5,7];
+// Merge array a & b
+let c = // Merge array a & b
+// sort array c
+
+
+// Q:12 - You have an array (see below) - out put a array with only 'name' e.g. [{name: 'xyz'}, {name: 'abc'}, {name: 'def}]
+let a = [{name: 'xyz', age: 13}, {name: 'abc', age: 16}, {name: 'def', age: 18}];
+
+// Q13: You have an array : arr = ['foo', 123, { name: 'Debi'} , 456, 'todo' ]. 
+// 1. Output an array with string types e.g. ['foo', 'todo']
+// 2. output an array with only object type
+// 3. output an array with only number type
+
+
+// Q:13 - What is clouser? Explain with example
+
+// Q14 - What is class and constructor? Exaplian with exaple
+
+// Q15: Take an array of numbers and sum them?
+
+// Q16: What is Array.reduce()? Explain well with example.
+
+// Q17: What is web components?
+
+
+// Q18: What is Template Literal? Explain with example
+
+// Q19: What is defferance between `==` & `===`? Expalin careully with an example.
+
+// Q20: let arr = [1,1,3,4,4,6,7,7,8,9,9]. Remove duplicate from array/
+
+// Q21: What is spread operator? Example?
+
+// Q22: Show an example of shallow copy/clone and object
+
+// Q23: What is REST parameter in ES6? Show with example.
+
+
+// Q24: what is deffieerance between Array.map() and Array().forEach methods
+
+
+// Q25: What is different HTTP verbs and how they are different from each other.
+
+
+
